a1b7ac3daab0898d4b8e120441b7e558983899d9
sholle <shorts@live.se>
2016-02-15 07:51:25 +0100

    Add PostService class

---
+++ swift-playground/swift-playground/PostService.swift
@@ -0,0 +1,17 @@
+//
+//  PostService.swift
+//  swift-playground
+//
+//  Created by viktor johansson on 15/02/16.
+//  Copyright © 2016 viktor johansson. All rights reserved.
+//
+
+import Foundation
+
+class PostService {
+    func getPosts() {
+        // Http-request
+        // Extract data from JSON
+        // Delegate data
+    }
+}
