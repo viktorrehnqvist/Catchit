328777824aedb57a3ecf13427ebeaee457132cdc
sholle <shorts@live.se>
2016-03-20 13:53:07 +0100

    Create and link achievementCollectionViewCells to corresponding controller

---
+++ swift-playground/swift-playground/AchievementCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  AchievementCollectionViewCell.swift
+//  swift-playground
+//
+//  Created by viktor johansson on 20/03/16.
+//  Copyright © 2016 viktor johansson. All rights reserved.
+//
+
+import UIKit
+
+class AchievementCollectionViewCell: UICollectionViewCell {
+    
+}
