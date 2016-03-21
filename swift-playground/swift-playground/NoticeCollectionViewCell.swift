10c90a4dd0f2415d3c2adf8eef6c5a42f7d49e16
sholle <shorts@live.se>
2016-03-21 21:09:03 +0100

    Layout notice view

---
+++ swift-playground/swift-playground/NoticeCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  NoticeCollectionViewCell.swift
+//  swift-playground
+//
+//  Created by viktor johansson on 21/03/16.
+//  Copyright © 2016 viktor johansson. All rights reserved.
+//
+
+import UIKit
+
+class NoticeCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var noticeLabel: UILabel!
+    @IBOutlet weak var noticeImage: UIImageView!
+    
+}
--- swift-playground/swift-playground/NoticeViewController.swift
+++ swift-playground/swift-playground/NoticeViewController.swift
@@ -7,28 +7,64 @@
 //
 
 import UIKit
+import Foundation
+import Alamofire
 
-class NoticeViewController: UIViewController {
-
+@available(iOS 9.0, *)
+class NoticeViewController:  UIViewController, PostServiceDelegate, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    let postService = PostService()
+    var screenSize: CGRect = UIScreen.mainScreen().bounds
+    
+    func setPosts(json: AnyObject) {
+        print(json)
+    }
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        postService.getPosts()
+        self.postService.delegate = self
+        // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    override func viewWillAppear(animated: Bool) {
+        self.navigationController?.navigationBarHidden = false
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 30
     }
-    */
-
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("noticeCell", forIndexPath: indexPath) as! NoticeCollectionViewCell
+        
+        cell.layer.shouldRasterize = true
+        cell.layer.rasterizationScale = UIScreen.mainScreen().scale
+        cell.layer.borderWidth = 1
+        cell.layer.borderColor = UIColor.lightGrayColor().CGColor
+        cell.noticeImage.image = UIImage(named: "avatar")
+        cell.noticeLabel.text! = "Mikael har kommenterat din post"
+        
+        return cell
+        
+    }
+    
+    func collectionView(collectionView: UICollectionView,
+        layout collectionViewLayout: UICollectionViewLayout,
+        sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+            let size = CGSize(width: screenSize.width, height: 50)
+            
+            return size
+    }
+    
+    
+    
 }
