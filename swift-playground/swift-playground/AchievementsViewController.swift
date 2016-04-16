2e6aac56931dbdb78be67e93876387e0c78d442e
sholle <shorts@live.se>
2016-04-16 17:22:46 +0200

    Refactor setAchievementData in achievements view controller

--- swift-playground/swift-playground/AchievementsViewController.swift
+++ swift-playground/swift-playground/AchievementsViewController.swift
@@ -43,45 +43,51 @@ class AchievementsViewController: UIViewController, AchievementServiceDelegate, 
                 achievementCompleterUserId.append((json[i]?["completer_user_infos"]!![0] as? ([Int]))!)
                 achievementCompleterUserNames.append((json[i]?["completer_user_infos"]!![1] as? ([String]))!)
                 achievementCompleterUserAvatars.append((json[i]?["completer_user_infos"]!![2] as? ([String]))!)
-                
+                let postImagesToLoad = json[i]["latest_posts"]!!.count
                 // Load first three postes for achievement
-                if json[i]["latest_posts"]!!.count > 0 {
-                    if let CompleterImageUrl = json[i]["latest_posts"]?![0] as? String {
-                        let url = NSURL(string: "http://localhost:3000" + CompleterImageUrl)!
+                for postIndex in 0...(postImagesToLoad - 1) {
+                    if let completerImageUrl = json[i]["latest_posts"]?![postIndex] as? String {
+                        print(json[i]["latest_posts"]?![postIndex])
+                        let url = NSURL(string: "http://localhost:3000" + completerImageUrl)!
                         let data = NSData(contentsOfURL:url)
                         if data != nil {
-                            achievementFirstCompleterImages.append(UIImage(data: data!)!)
+                            switch postIndex {
+                            case 0:
+                                print("First image for \(i) loaded")
+                                achievementFirstCompleterImages.append(UIImage(data: data!)!)
+                            case 1:
+                                print("Second image for \(i) loaded")
+                                achievementSecondCompleterImages.append(UIImage(data: data!)!)
+                            case 2:
+                                print("Third image for \(i) loaded")
+                                achievementThirdCompleterImages.append(UIImage(data: data!)!)
+                            default:
+                                print("Switch Error")
+                            }
                         }
                     }
-                } else {
-                    achievementFirstCompleterImages.append(noPostImage!)
                 }
-                if json[i]["latest_posts"]!!.count > 1 {
-                    if let CompleterImageUrl = json[i]["latest_posts"]?![1] as? String {
-                        let url = NSURL(string: "http://localhost:3000" + CompleterImageUrl)!
-                        let data = NSData(contentsOfURL:url)
-                        if data != nil {
-                            achievementSecondCompleterImages.append(UIImage(data: data!)!)
-                        }
+                var postsAlreadyLoaded = postImagesToLoad
+                while postsAlreadyLoaded < 3 {
+                    switch postsAlreadyLoaded {
+                    case 0:
+                        achievementFirstCompleterImages.append(noPostImage!)
+                        print("First image missing, adding!")
+                    case 1:
+                        achievementSecondCompleterImages.append(noPostImage!)
+                        print("Second image missing, adding!")
+                    case 2:
+                        achievementThirdCompleterImages.append(noPostImage!)
+                        print("Third image missing, adding!")
+                    default:
+                        print("Missing imageCount \(postsAlreadyLoaded)")
                     }
-                } else {
-                    achievementSecondCompleterImages.append(noPostImage!)
+                    postsAlreadyLoaded! += 1
                 }
-                if json[i]["latest_posts"]!!.count > 2 {
-                    if let CompleterImageUrl = json[i]["latest_posts"]?![2] as? String {
-                        let url = NSURL(string: "http://localhost:3000" + CompleterImageUrl)!
-                        let data = NSData(contentsOfURL:url)
-                        if data != nil {
-                            achievementThirdCompleterImages.append(UIImage(data: data!)!)
-                        }
-                    }
-                } else {
-                    achievementThirdCompleterImages.append(noPostImage!)
-                }
-
             }
         } else {
             moreAchievementsToLoad = false
+            achievementThirdCompleterImages.append(noPostImage!)
         }
         NSOperationQueue.mainQueue().addOperationWithBlock(collectionView.reloadData)
     }
