6c2cd4e4df93a314e08a0a2a89b08d2d61b664cd
sholle <shorts@live.se>
2016-05-16 10:33:11 +0200

    Refactor layout in Home

--- swift-playground/swift-playground/HomeViewController.swift
+++ swift-playground/swift-playground/HomeViewController.swift
@@ -193,7 +193,7 @@ class HomeViewController: UIViewController, PostServiceDelegate, UIScrollViewDel
         sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
             let image = self.postImages[indexPath.row]
             let heightFactor = image.size.height / image.size.width
-            let size = CGSize(width: 320, height: heightFactor * 320 + 160)
+            let size = CGSize(width: 320, height: heightFactor * 320 + 150)
         
             return size
     }
