bb52307b1a2d15d5da5796eeb43e4848f84886bb
sholle <shorts@live.se>
2016-05-20 19:06:06 +0200

    Search view fetching autocomplete from api

---
+++ swift-playground/swift-playground/SearchService.swift
@@ -0,0 +1,39 @@
+//
+//  SearchService.swift
+//  Catchit
+//
+//  Created by viktor johansson on 20/05/16.
+//  Copyright © 2016 viktor johansson. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+// MARK: Protocols
+protocol SearchServiceDelegate {
+    func setSearchResult(json: AnyObject)
+}
+
+class SearchService {
+    
+    // MARK: Setup
+    var delegate: SearchServiceDelegate?
+    let headers = NSUserDefaults.standardUserDefaults().objectForKey("headers") as? [String : String]
+    let url = NSUserDefaults.standardUserDefaults().objectForKey("url")! as! String
+
+    // MARK: GET-Requests
+    func search(searchString: String) {
+        Alamofire.request(.GET, url + "users/autocomplete_user_name?term=" + searchString, headers: headers)
+            .responseJSON { response in
+                if let JSON = response.result.value {
+                    if self.delegate != nil {
+                        dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                            self.delegate?.setSearchResult(JSON)
+                        })
+                    }
+                }
+                
+        }
+    }
+    
+}
--- swift-playground/swift-playground/SearchViewController.swift
+++ swift-playground/swift-playground/SearchViewController.swift
@@ -8,21 +8,47 @@
 
 import UIKit
 
-class SearchViewController: UIViewController, UITextFieldDelegate, UICollectionViewDelegate, UICollectionViewDataSource {
+class SearchViewController: UIViewController, UITextFieldDelegate, UICollectionViewDelegate, UICollectionViewDataSource, SearchServiceDelegate {
     
     // MARK: Setup
     let postService = PostService()
+    let searchService = SearchService()
     @IBOutlet weak var searchField: UITextField!        
     @IBOutlet weak var collectionView: UICollectionView!
+    let defaultAvatar = UIImage(named: "avatar")
+    let lockIcon = UIImage(named: "lock_icon")
     var screenSize: CGRect = UIScreen.mainScreen().bounds
-    var resultCount: Int = 1
+    var resultCount: Int = 0
+    
+    var labels: [String] = []
+    var ids: [Int] = []
+    var types: [String] = []
     
     // MARK: Lifecycle
+    func setSearchResult(json: AnyObject) {
+        labels = []
+        ids = []
+        types = []
+        resultCount = json.count
+        if json.count > 0 {
+            for i in 0...(json.count - 1) {
+                labels.append(json[i]["label"] as! String)
+                ids.append(Int(json[i]?["id"] as! String)!)
+                if i == 1 {
+                    types.append("user")
+                } else {
+                    types.append("achievement")
+                }
+            }
+        }
+        NSOperationQueue.mainQueue().addOperationWithBlock(collectionView.reloadData)
+    }
     
     // MARK: View Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView.delegate = self
+        searchService.delegate = self
         searchField.delegate = self
         searchField.becomeFirstResponder()
         if screenSize.width < 400 {
@@ -46,11 +72,23 @@ class SearchViewController: UIViewController, UITextFieldDelegate, UICollectionV
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         
         let cell = collectionView.dequeueReusableCellWithReuseIdentifier("cell", forIndexPath: indexPath) as! SearchCollectionViewCell
-    
+        
+        if types[indexPath.row] == "user" {
+            cell.resultButton.setTitle("Följ", forState: .Normal)
+            cell.bucketlistButton.hidden = true
+            cell.resultIcon.image = defaultAvatar
+        } else {
+            cell.resultButton.setTitle("Ladda upp", forState: .Normal)
+            //cell.bucketlistButton.hidden = false
+            cell.resultIcon.image = lockIcon
+        }
         cell.layer.shouldRasterize = true
         cell.layer.rasterizationScale = UIScreen.mainScreen().scale
+        cell.resultLabel.text! = labels[indexPath.row]
         cell.resultButton.layer.cornerRadius = 5
+        cell.bucketlistButton.tag = indexPath.row
         
+            
         return cell
         
     }
@@ -60,6 +98,16 @@ class SearchViewController: UIViewController, UITextFieldDelegate, UICollectionV
     }
     
     // MARK: User Interaction
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        let recordId = ids[indexPath.row]
+        if types[indexPath.row] == "achievement" {
+            self.performSegueWithIdentifier("showAchievementFromSearch", sender: recordId)
+        } else {
+            self.performSegueWithIdentifier("showProfileFromSearch", sender: recordId)
+        }
+    }
+    
+    
     @IBAction func back(sender: AnyObject) {
         self.navigationController?.popViewControllerAnimated(false)
     }
@@ -68,8 +116,25 @@ class SearchViewController: UIViewController, UITextFieldDelegate, UICollectionV
         self.view.endEditing(true)
     }
 
+    @IBAction func textDidChange(sender: AnyObject) {
+        searchService.search(searchField.text!)
+    }
+    
     // MARK: - Navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        let backItem = UIBarButtonItem()
+        backItem.title = "Tillbaka"
+        navigationItem.backBarButtonItem = backItem // This will show in the next view controller being pushed
+        
+        if segue.identifier == "showAchievementFromSearch" {
+            let vc = segue.destinationViewController as! ShowAchievementViewController
+            vc.achievementId = sender?.integerValue
+        }
+        
+        if segue.identifier == "showProfileFromSearch" {
+            let vc = segue.destinationViewController as! ProfileViewController
+            vc.userId = sender?.integerValue
+        }
     }
 
 }
